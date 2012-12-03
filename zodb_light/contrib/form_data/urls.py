2394e859a8e64eb861b343dd117ff6db058618af
jpic <jamespic@gmail.com>
2012-12-03 10:54:12 +0100

    PEP8

--- zodb_light/contrib/form_data/urls.py
+++ zodb_light/contrib/form_data/urls.py
@@ -5,10 +5,10 @@ import zodb_light
 import views
 
 urlpatterns = patterns('',
-    url(r'(?P<uuid>'+zodb_light.UUID_REGEXP+')/$',
+    url(r'(?P<uuid>' + zodb_light.UUID_REGEXP + ')/$',
         views.FormDataDetailView.as_view(),
         name='form_data_form_data_detail'),
-    url(r'(?P<uuid>'+zodb_light.UUID_REGEXP+')/update/$',
+    url(r'(?P<uuid>' + zodb_light.UUID_REGEXP + ')/update/$',
         views.FormDataUpdateView.as_view(),
         name='form_data_form_data_update'),
 )
